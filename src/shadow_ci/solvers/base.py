d658f3e4c86a52a30b9aea4a01153056716fc3e2
maxmarvell <max.marvell@hotmail.co.uk>
2025-11-17 15:42:17 +0000

    typing fixes

--- src/shadow_ci/solvers/base.py
+++ src/shadow_ci/solvers/base.py
@@ -1,12 +1,15 @@
 from abc import ABC, abstractmethod
-from typing import Tuple, Union
+from typing import Tuple, Union, Optional
 from qiskit_nature.second_q.operators import FermionicOp
 from qiskit.quantum_info import Statevector
 from pyscf import scf
-
+import numpy as np
 
 class GroundStateSolver(ABC):
     """Abstract base class for quantum state solvers."""
+
+    state: Optional[Statevector]
+    energy: Optional[np.ndarray]
 
     def __init__(self, mf: Union[scf.hf.RHF, scf.uhf.UHF]):
         self.mf = mf
