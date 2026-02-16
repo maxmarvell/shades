360094532ac03f70a9260293f154b74276655288
Max Marvell <max.marvell@hotmail.co.uk>
2026-02-16 15:25:31 +0000

    Remove test files

--- tests/test.py
+++
@@ -1,8 +0,0 @@
-from shades.stabilizer import StabilizerState
-import numpy as np
-
-s = StabilizerState(np.array([[1,-1]]))
-ham = [[1, 'X']]
-spawned_states = s.apply_hamiltonian(ham)
-for s in spawned_states:
-     print(s.generator_matrix, spawned_states[s])
